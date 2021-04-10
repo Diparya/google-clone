3054c5a8ee9a41751754e1c2e481cc55d072fb96
Dipesh <dipesharya28@gmail.com>
2021-04-10 14:42:45 +0530

    app build

---
+++ keys.js
@@ -0,0 +1,5 @@
+// https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = "AIzaSyCQmuzM5CaA_2p6XC-T-72Y6vt34lZY278";
+
+// https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = "7a31dbcb8512c7855"; // << Replace with your own CX key
